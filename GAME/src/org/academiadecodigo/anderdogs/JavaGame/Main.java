306728e91a62cefc9aeabaa1ce6034b08cd8c8e9
aduque <aphous1@gmail.com>
2020-10-10 17:47:35 +0100

    Ola

--- GAME/src/org/academiadecodigo/anderdogs/JavaGame/Main.java
+++ GAME/src/org/academiadecodigo/anderdogs/JavaGame/Main.java
@@ -3,5 +3,6 @@ package org.academiadecodigo.anderdogs.JavaGame;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hey this is a test to push to Git Hub");
+        System.out.println("OLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
     }
 }
