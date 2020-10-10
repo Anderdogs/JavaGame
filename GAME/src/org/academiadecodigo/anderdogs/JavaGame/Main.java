fa4066ae7b3afdda20dd2f00c13a4d50064d3681
Bruno Vilar <bruno.p.vilar.code@gmail.com>
2020-10-10 16:35:12 +0100

    Third commit - Teste Github push 3

--- GAME/src/org/academiadecodigo/anderdogs/JavaGame/Main.java
+++ GAME/src/org/academiadecodigo/anderdogs/JavaGame/Main.java
@@ -1,4 +1,7 @@
 package org.academiadecodigo.anderdogs.JavaGame;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hey this is a test to push to Git Hub");
+    }
 }
